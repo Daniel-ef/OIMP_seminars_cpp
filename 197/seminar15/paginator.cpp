2bc64dd59717d98289fa4b7106602a9173b95d12
Daniil Efimov <dlefimov@yandex-team.ru>
2020-03-04 12:53:31 +0300

    seminar15 (2)

--- 197/seminar15/paginator.cpp
+++ 197/seminar15/paginator.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <vector>
 
+template <typename IterT>
 class Paginator {
+public:
+  class PageIterator {
 
+  };
+
+  Paginator(const IterT& begin, const IterT& end, size_t page_size) :
+  begin_(begin), end_(end), page_size(page_size) {}
+
+  PageIterator begin() {
+    return PageIterator(begin_, begin_ + page_size);
+  }
+
+  PageIterator end() {
+    return PageIterator(end_, end_);
+  }
+
+  operator++() {
+    ++cur_page;
+  }
+private:
+  IterT begin_;
+  IterT end_;
+  size_t page_size;
+  size_t cur_page{0};
 };
 
 template <typename ContainerT>
@@ -15,11 +40,12 @@ auto make_paginator(const ContainerT& container,
 }
 
 int main() {
-  auto paginator = make_paginator({1, 2, 3, 4}, 10);
+  auto container = std::vector<int>{1, 2, 3, 4};
+  auto paginator = make_paginator(container, 2);
 
-  for (auto page : paginator) {
-    for (auto doc : page) {
-      std::cout << doc << std::endl;
+  for (auto row : paginator) {
+    for (auto element : row) {
+      std::cout << element << std::endl;
     }
   }
 }
